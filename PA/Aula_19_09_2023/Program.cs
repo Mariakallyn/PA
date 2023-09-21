9d953ec8767d1c6fe9dcb57a14770d765bfcfb16
Aluno <aluno@etec.sp.gov.br>
2023-09-21 20:46:50 -0300

    Atividades Aula21/09/2023

--- PA/Aula_19_09_2023/Program.cs
+++ PA/Aula_19_09_2023/Program.cs
@@ -6,10 +6,53 @@ namespace Aula_19_09_2023
     {
         static void Main(string[] args)
         {
-            float nota1, nota2, nota3, nota4;
+            float nota1, nota2, nota3, nota4, soma, media;
 
             Console.Write("Informe a 1º Nota: ");
             nota1 = float.Parse(Console.ReadLine());
+
+            Console.Write("informe a 2 nota: ")
+            nota2 = float.Parse(Console.ReadLine());
+
+            Console.Write("informe a 3 nota: ")
+            nota3 = float.Parse(Console.ReadLine());
+
+            Console.Write("informe a 4 nota: ")
+             nota4 = float.Parse(Console.ReadLine());
+
+            soma = nota1 + nota2 + nota3 + nota4;
+
+            media = soma / 4;
+
+            //0.00 a 4.99 - Reprovado
+            //5.00 a 6.99 - Recuperacao
+            //7.00 a 10 - Aprovado
+
+
+            if (media < 5)
+            {
+                Console.WriteLine("Reprovado");
+                Console.WriteLine("Poxa que pena!!!");
+
+            }
+            else if (media < 7)
+            {
+                Console.WriteLine("Recuperacao");
+                Console.WriteLine("Coragem!!!");
+                {
+
+                else    
+                }
+
+                Console.WriteLine("Aprovado");
+                Console.WriteLine("Parabens!!!");
+
+            }
+
+
+
+
+
         }
     }
 }
